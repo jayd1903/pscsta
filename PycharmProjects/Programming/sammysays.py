9171289167ce2eaf995b3e5142aca698f6134f72
jayd1903 <jayd1903@gmail.com>
2017-10-19 18:20:36 -0700

    hi

--- PycharmProjects/Programming/sammysays.py
+++ PycharmProjects/Programming/sammysays.py
@@ -3,7 +3,7 @@ b = a.readlines()
 l=int(b[0].strip())
 for x in range(1,l+1):
     if b[x].find("Sammy says") == 0:
-        s = b[x].replace("Sammy says", "")
+        s = b[x].replace("Sammy says", "") #
         print(s.strip())
     else:
         pass
