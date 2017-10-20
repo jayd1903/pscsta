58058aea1611b11f0f0b4bbdcd0316631cfbc659
jayd1903 <jayd1903@gmail.com>
2017-10-19 18:21:23 -0700

    hi

--- PycharmProjects/Programming/clockpaperscissors.py
+++ PycharmProjects/Programming/clockpaperscissors.py
@@ -3,7 +3,7 @@ b = a.readlines()
 l=int(b[0].strip())
 for x in range(1,l+1):
     z = b[x].split()
-    if int(z[0]) + int(z[1]) + int(z[2])>1:
+    if int(z[0]) + int(z[1]) + int(z[2])>1: #hi
         print("Player 1")
     else:
         print("Player 2")
